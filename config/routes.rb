373a84c201ac8b0e51a7ffe91e33fa5b76c142d0
motsu8e <motsu159357@gmail.com>
2024-03-10 21:59:39 +0900

    mock complete

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # localhost:3000/index, new
+  resources :posts, only: [:index, :new, :create, :edit, :update, :destroy]
 end
